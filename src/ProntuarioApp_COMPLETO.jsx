aeedf8aad396fd180736b574d161c5fdf6c25ef7
podabrasil <podabrasil.comercial@gmail.com>
2025-05-10 13:46:13 -0300

    Delete src/ProntuarioApp_COMPLETO.jsx

--- src/ProntuarioApp_COMPLETO.jsx
+++
@@ -1,8 +0,0 @@
-
-import { useState } from "react";
-
-// [conteúdo do componente completo omitido aqui para brevidade]
-
-export default function ProntuarioApp() {
-  // Conteúdo igual ao gerado antes com todos os campos do checklist completo
-}
